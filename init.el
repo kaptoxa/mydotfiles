e3d99f7fa423b6ff735eb2104d3bff47ecee5740
Alex Blednov <alex.blednov@gmail.com>
2021-09-08 17:18:27 +0500

    solve conflict

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (defconst old-gc-cons-threshold gc-cons-threshold)
 (setq gc-cons-threshold most-positive-fixnum)
 (add-hook
@@ -23,29 +22,3 @@
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name (locate-user-emacs-file "myinit.org")))
-=======
-(defconst old-gc-cons-threshold gc-cons-threshold)
-(setq gc-cons-threshold most-positive-fixnum)
-(add-hook
- 'emacs-startup-hook
- (lambda ()
-   (let ((startup-time (float-time (time-subtract after-init-time before-init-time))))
-     (message "Emacs ready in %.2f seconds with %d garbage collections." startup-time gcs-done))
-   (setq gc-cons-threshold old-gc-cons-threshold)))
-
-(require 'package)
-
-(setq package-archives
-      '(("gnu" . "http://elpa.gnu.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")
-        ("org" . "http://orgmode.org/elpa/")
-        ("melpa-stable" . "https://stable.melpa.org/packages/")))
-
-(package-initialize)
-
-(when (not (package-installed-p 'use-package))
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(org-babel-load-file (expand-file-name (locate-user-emacs-file "myinit.org")))
->>>>>>> c05dafc7a604caf62c0c9b23cca1784760b8e41b

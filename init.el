5086e2dd2d9ab7012a7b9aaeb3a9f69124811d82
Alex Blednov <alex.blednov@gmail.com>
2021-12-10 15:54:33 +0500

    fix double melpa error

--- init.el
+++ init.el
@@ -11,7 +11,6 @@
 
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")
         ("org" . "http://orgmode.org/elpa/")
         ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
